e587c5f7f7d16f84e576dd67e561e8017562c2e7
Sergiy Leshchak <cryptoleschak73@gmail.com>
2023-01-20 03:18:57 +0300

    New

--- js/model.js
+++ js/model.js
@@ -1,3 +1,65 @@
-const model = 15;
+// const model = 125;
 
-console.log(model)
+// console.log(model)
+// let qwontity = prompt('How many brain wi haw');
+// qwontity = Number(qwontity)
+// console.log(qwontity);
+// console.log(typeof qwontity);
+
+// let elementWidth = '50.44px';
+// const result = Number.parseInt(elementWidth)
+
+// console.log('elementWidth', result)
+// console.log(typeof result)
+
+// let salary = 134.456789
+// salary = Number(salary.toFixed(3))
+
+// console.log(typeof salary)
+
+// console.log(salary)
+
+// const base = 5;
+// const power = 10;
+
+// const result = Math.pow(base, power)
+
+// console.log(result)
+// console.log(typeof result)
+
+// console.log(base ** power )
+
+// let base = prompt('Дай чесло');
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt('Дай степень');
+// power = Number(power);
+// console.log(power);
+
+// const result = (base ** power);
+// console.log(result)
+
+// const min = 1;
+// const max = 5000;
+// const result = Math.round(Math.random() * (max - min) + min)
+
+// console.log(result);
+
+
+// let brand = prompt('Дай бренд');
+// brand = brand.toLowerCase();
+
+// console.log(brand);
+
+
+const balance = false;
+let massage;
+
+if (balance > 0) {
+    massage = 'Позитивний баланц';
+} else {
+    massage = 'Негативний баланц';
+}
+
+console.log(massage)
